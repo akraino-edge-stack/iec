3d98320f21deeda0c0073fc93f9aa4e452ba0c0b
Ciprian Barbu <ciprian.barbu@enea.com>
2019-02-21 19:27:18 +0200

    Openstack Heat templates for IEC deployment
    Change-Id: Iadec664b9108639c8e62c4af41a8bf4aff80e305
    Signed-off-by: Ciprian Barbu <ciprian.barbu@enea.com>

---
+++ templates/k8s_master_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+apt update
+pwd
+# Looks like cloud-init does not set $HOME, so we can hack it into thinking it's /root
+HOME=${HOME:-/root}
+export HOME
+git clone https://gerrit.akraino.org/r/iec
+cd iec/scripts
+./k8s_common.sh
+./k8s_master.sh k8_master_ip k8_pod_net_cidr k8_svc_net_cidr
+. ${HOME}/.profile
+./cni/calico/calico.sh k8_cluster_ip
+token=$(kubeadm token list --skip-headers | tail -1 | awk '{print $1}')
+shaid=$(openssl x509 -in /etc/kubernetes/pki/ca.crt -noout -pubkey | openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | cut -d ' ' -f1)
+echo "kubeadm join k8_master_ip:6443 --token $token --discovery-token-ca-cert-hash sha256:$shaid" > /home/k8s_user/joincmd
+cat /home/k8s_user/joincmd
+./nginx.sh
+./helm.sh
