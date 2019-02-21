1fd8cc59fa7346b0908bbd1085068a642cf75843
Ciprian Barbu <ciprian.barbu@enea.com>
2019-02-21 19:27:18 +0200

    Openstack Heat templates for IEC deployment
    Change-Id: Iadec664b9108639c8e62c4af41a8bf4aff80e305
    Signed-off-by: Ciprian Barbu <ciprian.barbu@enea.com>

---
+++ templates/k8s_slaves_init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+echo "K8s Master IP is k8s_master_ip"
+apt update
+apt install sshpass
+pwd
+git clone https://gerrit.akraino.org/r/iec
+cd iec/scripts
+./k8s_common.sh
+joincmd=$(sshpass -p k8s_password ssh -o StrictHostKeyChecking=no k8s_user@k8s_master_ip 'for i in {1..300}; do if [ -f /home/ubuntu/joincmd ]; then break; else sleep 1; fi; done; cat /home/ubuntu/joincmd')
+eval sudo $joincmd
