f71c68ffd6cb9277d70ac03ae83e0d66cf124ec4
Ciprian Barbu <ciprian.barbu@enea.com>
2019-03-26 17:43:40 +0200

    Fix broken kubernetes-cni dependency
    Change-Id: If6cb5085c51db746498343012bd6475ea11292d4
    Signed-off-by: Ciprian Barbu <ciprian.barbu@enea.com>

--- src/foundation/scripts/k8s_common.sh
+++ src/foundation/scripts/k8s_common.sh
@@ -2,6 +2,7 @@
 
 DOCKER_VERSION=18.06.1~ce~3-0~ubuntu
 KUBE_VERSION=1.13.0-00
+K8S_CNI_VERSION=0.6.0-00
 
 # Install Docker as Prerequisite
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -25,6 +26,8 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt update
+# Minor fix for broken kubernetes-cni dependency in upstream xenial repo
+sudo apt install -y kubernetes-cni=${K8S_CNI_VERSION}
 sudo apt install -y \
   kubelet=${KUBE_VERSION} kubeadm=${KUBE_VERSION} kubectl=${KUBE_VERSION}
 sudo apt-mark hold kubelet kubeadm kubectl
