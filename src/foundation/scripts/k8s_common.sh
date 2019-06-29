c03b2b30693f257f464b175b63afbd14bb8d0468
Jingzhao.Ni <Jingzhao.Ni@arm.com>
2019-06-29 15:01:57 +0800

    Add Net verification script before install softwar
    In CI task, the docker-ce install failed occasionally. The reason
    is that the host can not resolve the download.docker.com URL. So,
    in this patch, the verification script was added into scripts for
    avoiding the issue.
    
    Signed-off-by: Jingzhao.Ni <Jingzhao.Ni@arm.com>
    Change-Id: I67c870203e5e80b0fe3de7ff33e71011ac8d7bf9

--- src/foundation/scripts/k8s_common.sh
+++ src/foundation/scripts/k8s_common.sh
@@ -4,6 +4,41 @@ DOCKER_VERSION=18.06.1~ce~3-0~ubuntu
 KUBE_VERSION=1.13.0-00
 K8S_CNI_VERSION=0.6.0-00
 
+ping_success_status() {
+  if ping -c 1 $1 >/dev/null; then
+    echo "$1 Ping is successful."
+    continue
+  fi
+}
+
+refresh_dns(){
+  sudo apt update
+  sudo apt install -y nscd
+  sudo /etc/init.d/nscd restart
+  echo "sleep 10s ..."
+  sleep 10
+  break
+}
+
+check_network(){
+
+  IP_LIST="www.google.com download.docker.com"
+
+  for IP in $IP_LIST; do
+    ping_success_status $IP
+    echo "$IP ping is failed 1st times"
+    ping_success_status $IP
+    echo "$IP ping is failed 2ed times"
+    ping_success_status $IP
+    echo "$IP Ping is failure!"
+    refresh_dns
+  done
+
+}
+
+# Before install enssential software, make sure the network is OK
+check_network
+
 # Install basic software
 sudo apt update
 sudo apt install -y software-properties-common apt-transport-https curl
