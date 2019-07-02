b21fa9be6adf78f5c6fb59bc4771e346d04c2932
Ciprian Barbu <ciprian.barbu@enea.com>
2019-07-02 13:48:16 +0300

    Add PONSim support
    Also refactored the existing BBSim installation scripts and re-organized
    the scripts into separate bbsim and ponsim folders
    
    Signed-off-by: Ciprian Barbu <ciprian.barbu@enea.com>
    Change-Id: I114169cfbcee70985eb098561491ea341b26e4b2

---
+++ src/use_cases/seba_on_arm/test/bbsim/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+# shellcheck disable=SC2016
+
+basepath=$(cd "$(dirname "$0")"; pwd)
+BBSIM_VERSION=${BBSIM_VERSION:-1.0.0}
+CORD_CHART=${CORD_CHART:-${basepath}/../../src_repo/seba_charts}
+
+source ${basepath}/../util.sh
+
+# Install bbsim
+helm install -n bbsim --version ${BBSIM_VERSION} ${CORD_CHART}/bbsim
+wait_for 300 'test $(kubectl get pods | grep -vcE "(\s(.+)/\2.*Running|bbsim.*Running)") -eq 1' || true
--- src/use_cases/seba_on_arm/test/bbsim/test.sh
+++ src/use_cases/seba_on_arm/test/bbsim/test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -ex
 # shellcheck disable=SC2016
 
-#Modified from https://github.com/cachengo/seba_charts/blob/master/scripts/mini_test.sh
-
 TOSCA_POD=`kubectl get pods | grep xos-tosca | cut -d " " -f1`
 TOSCA_IP=`kubectl describe pod $TOSCA_POD | grep Node: | cut -d "/" -f2`
 BBSIM_IP=`kubectl get services -n voltha | grep bbsim | tr -s ' ' | cut -d " " -f3`
