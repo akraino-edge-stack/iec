8d1088c6454d5d7463226e192920800daeef23ed
Jianlin Lv <Jianlin.Lv@arm.com>
2019-10-28 13:53:58 +0800

    Integrate nokia/danm CNI into IEC
    Make master and work nodes support danm deployment
    1. Implement support for danm CNI in setup-cni.sh;
    2. Wrap the work node initialization function before calling setup-cni.sh;
    3. Add danm CNI deployment scripts and relevant config file.
    
    Signed-off-by: Jianlin Lv <Jianlin.Lv@arm.com>
    Change-Id: I19564d0c1bdb183c4328d921eaab8d873f064e07

---
+++ src/foundation/scripts/cni/danm/danm_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o xtrace
+set -e
+
+SCRIPTS_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+echo "SCRIPTS_DIR is :$SCRIPTS_DIR"
+
+# Get binary
+wget -P ${SCRIPTS_DIR} https://github.com/iecedge/danm-binary/releases/download/v4.0.0/danm
+wget -P ${SCRIPTS_DIR} https://github.com/iecedge/danm-binary/releases/download/v4.0.0/fakeipam
+chmod +x ${SCRIPTS_DIR}/danm ${SCRIPTS_DIR}/fakeipam
+
+# Copy binary into CNI plugin directory
+cp -f ${SCRIPTS_DIR}/danm /opt/cni/bin
+cp -f ${SCRIPTS_DIR}/fakeipam /opt/cni/bin
+
+# Put DANM config file into CNI configuration directory
+cp -f ${SCRIPTS_DIR}/00-danm.conf /etc/cni/net.d/
