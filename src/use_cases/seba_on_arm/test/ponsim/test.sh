b4c318d78023187a41af2f04bb540979039c3b42
Ciprian Barbu <ciprian.barbu@enea.com>
2020-04-02 06:24:33 +0300

    Update testing tools for SEBA validation
    This updates the Dockerfile that represents the running environment of
    cord-tester. The major difference is moving from xenial to bionic base
    image.
    
    It also updates the testing scripts according to the changes in
    cord-tester and fixes a few issues with running cord-tester from a
    Docker container.
    
    JIRA: IEC-35
    
    Signed-off-by: Ciprian Barbu <ciprian.barbu@enea.com>
    Change-Id: I2a3c2175357ebce802111231c87f1f9374978f8c

--- src/use_cases/seba_on_arm/test/ponsim/test.sh
+++ src/use_cases/seba_on_arm/test/ponsim/test.sh
@@ -5,7 +5,7 @@ set -ex
 
 basepath=$(cd "$(dirname "$0")"; pwd)
 
-CORD_IMG="${CORD_IMG:-iecedge/cord-tester:latest}"
+CORD_IMG="${CORD_IMG:-iecedge/cord-tester:cord-7.0}"
 KUBE_DIR="${KUBE_DIR:-${PWD}/.kube}"
 K8S_MASTER_IP="${K8S_MASTER_IP:-127.0.0.1}"
 TEST_USER="${TEST_USER:-ubuntu}"
@@ -34,7 +34,11 @@ DOCKER_CMD="docker run -id -e K8S_MASTER_IP=${K8S_MASTER_IP} \
 if cont_id=$(eval "${DOCKER_CMD}")
 then
   echo "Starting SIAB.robot in ${cont_id}"
-  docker exec "${cont_id}" /workspace/docker_run.sh
+  docker exec "${cont_id}" sudo cp /workspace/docker_run.sh \
+        "/home/${TEST_USER}/docker_run.sh"
+  docker exec "${cont_id}" sudo chown "${TEST_USER}:${TEST_USER}" \
+         "/home/${TEST_USER}/docker_run.sh"
+  docker exec "${cont_id}" "/home/${TEST_USER}/docker_run.sh"
 else
   echo "Failed to execute docker command ${cont_id}"
   exit 1
