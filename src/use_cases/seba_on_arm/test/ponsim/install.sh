12b360b18cea8e3f2cb85ec9bbe047c62945431a
Ciprian Barbu <ciprian.barbu@enea.com>
2020-07-16 02:18:03 +0300

    Export SEBAVALUES for known projects
    This helps automation-tools/seba-in-a-box configure the helm-charts
    accordingly to the environment where it is running. For the known
    projects iec and rec, the difference is primarily the underlying
    default cni, calico and flannel respectively
    
    Signed-off-by: Ciprian Barbu <ciprian.barbu@enea.com>
    Change-Id: Id7090a8b0dfc27318ee92bd95d701287120a22ef

--- src/use_cases/seba_on_arm/test/ponsim/install.sh
+++ src/use_cases/seba_on_arm/test/ponsim/install.sh
@@ -16,9 +16,11 @@ if [ "$#" -gt 0 ]; then UPSTREAM_PROJECT="$1"; fi
 case "${UPSTREAM_PROJECT}" in
   "iec")
     HELM_CHARTS_REV="${HELM_CHARTS_REV_IEC}"
+    SEBAVALUES="configs/seba-ponsim-iec-arm64.yaml"
     ;;
   "rec")
     HELM_CHARTS_REV="${HELM_CHARTS_REV_REC}"
+    SEBAVALUES="configs/seba-ponsim-rec-arm64.yaml"
     ;;
   *)
     echo "Invalid upstream project ${UPSTREAM_PROJECT}"
@@ -28,7 +30,7 @@ case "${UPSTREAM_PROJECT}" in
 esac
 
 export M="/tmp/milestones"
-export SEBAVALUE=
+export SEBAVALUES=
 export WORKSPACE="${HOME}"
 export HELM_CHARTS_REV
 
