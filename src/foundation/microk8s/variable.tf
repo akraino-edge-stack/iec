b76ec24f73c8e301659a03202957f0c94dfd76e8
ashgit301 <ashvin.p@bluemeric.com>
2021-05-17 16:02:12 +0530

    terraform + microk8s
    Signed-off-by: ashgit301 <ashvin.p@bluemeric.com>
    Change-Id: I087bb513794030c938f93350ebaaabf200474dcb

---
+++ src/foundation/microk8s/variable.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "aws_region"
+  default     = "us-east-2"
+}
+
+variable "aws_instance" {
+  description = "instance_type"
+  default     = "t2.small"
+}
+
+variable "aws_ami" {
+  description = "aws_ami"
+  default     = "ami-01e7ca2ef94a0ae86"
+}
+
+
+
