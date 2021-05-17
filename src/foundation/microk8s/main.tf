fca01725a52b5e0729bdaa004afe5ad1a19e6760
ashgit301 <ashvin.p@bluemeric.com>
2021-05-17 15:36:30 +0530

    terraform
    Change-Id: Id8d633f453ca4509a9a14c5bc25504f1918b122a
    Signed-off-by: ashgit301 <ashvin.p@bluemeric.com>

---
+++ src/foundation/microk8s/main.tf
@@ -0,0 +1,124 @@
+provider "aws" {
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "master" {
+  ami           = var.aws_ami
+  instance_type = var.aws_instance
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo su
+              apt update -y >> microk8s_install.log
+              apt install snapd -y >> microk8s_install.log
+              snap install core >> microk8s_install.log
+              export PATH=$PATH:/snap/bin
+              snap install microk8s --classic >> microk8s_install.log
+              microk8s status --wait-ready
+              microk8s enable dns >> microk8s_install.log
+              microk8s add-node > microk8s.join_token
+              microk8s config > configFile
+              EOF
+  key_name = "terraform"
+  tags = {
+    Name = "master"
+  }
+  provisioner "remote-exec" {
+  inline = ["until [ -f /microk8s.join_token ]; do sleep 5; done; cat /microk8s.join_token",
+            "sudo sed -i 's/#MOREIPS/IP.7 = ${self.public_ip}\\n#MOREIPS/g' /var/snap/microk8s/current/certs/csr.conf.template",
+            "sudo sleep 1m",
+            "sudo microk8s stop",
+            "sudo microk8s start"
+           ]
+  }
+
+  connection {
+    host = self.public_ip
+    type     = "ssh"
+    user     = "ubuntu"
+    password = ""
+    private_key = "${file("terraform.pem")}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+               ssh-keyscan -H ${self.public_dns} >> ~/.ssh/known_hosts
+               scp -i terraform.pem ubuntu@${self.public_dns}:/microk8s.join_token .
+               tail -n1 microk8s.join_token >> token
+               scp -i terraform.pem ubuntu@${self.public_dns}:/configFile .
+              EOT
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "master_microk8s"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+data "local_file" "foo" {
+  filename = "token"
+  depends_on = [
+    aws_instance.master,
+  ]
+}
+
+
+locals {
+    public_dns = aws_instance.master.public_dns
+    join = data.local_file.foo.content
+}
+
+
+resource "aws_instance" "worker" {
+  ami           = var.aws_ami
+  instance_type = var.aws_instance
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = templatefile("worker_user_data.tmpl", { token = local.join } )
+  key_name = "terraform"
+  tags = {
+    Name = "worker"
+  }
+
+  provisioner "remote-exec" {
+  inline = ["until [ -f /microk8s.complete ]; do sleep 5; done"]
+  }
+
+  connection {
+    host = self.public_ip
+    type     = "ssh"
+    user     = "ubuntu"
+    password = ""
+    private_key = "${file("terraform.pem")}"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [
+    aws_instance.master,
+  ]
+}
+
+
+output "master_ip" {
+  value         = aws_instance.master.public_ip
+}
+
---
+++ src/foundation/microk8s/variable.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  description = "aws_region"
+  default     = "us-east-2"
+}
+
+variable "aws_instance" {
+  description = "instance_type"
+  default     = "t2.small"
+}
+
+variable "aws_ami" {
+  description = "aws_ami"
+  default     = "ami-01e7ca2ef94a0ae86"
+}
+
+variable "access_key" {
+  description = "access_key"
+  default     = "AKIAY4UPZOCVUNW6T6HN"
+}
+
+variable "secret_key" {
+  description = "secret_key"
+  default     = "rSkiZVGul8iudFL/yJza3l9uJRzoY6Xuim54fb1a"
+}
+
+
