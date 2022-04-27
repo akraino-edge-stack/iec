46547488571cd0b669de61e9192b3479c92089b8
rd-sw <rd-sw@ysemi.cn>
2022-04-27 13:02:07 +0000

    ROBOX：Upload robox scripts for IEC release 6
    RT
    
    Signed-off-by: rd-sw <rd-sw@ysemi.cn>
    Change-Id: I1695c71cb291273b926370bc9a84528107663ef0

---
+++ src/use_cases/robox/deploy_robox.sh
@@ -0,0 +1,160 @@
+#########################################################################
+# File Name: deploy_robox.sh
+# Author   : rd-sw
+# email    : rd-sw@ysemi.cn
+# Created Time: 2022-04-27 10:25
+#########################################################################
+#!/bin/bash
+
+INSTANCE_NUM=4
+ROBOX_TEMPLATE_YAML=$(cat <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: anbox
+spec:
+  selector:
+    matchLabels:
+      app: android
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: android
+    spec:
+      containers:
+      - name: android
+        image: android:robox
+        resources:
+          requests:
+            cpu: 1
+            memory: 1024Mi
+          limits:
+            cpu: 4
+            memory: 4096Mi
+        env:
+        - name: ANDROID_NAME
+          value: "xbox"
+        - name: PATH
+          value: "/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin"
+        - name: ANDROID_ROOT
+          value: "/"
+        - name: ANDROID_DATA
+          value: "/data"
+        - name: DOWNLOAD_CACHE
+          value: "/data/cache"
+        - name: LOGNAME
+          value: "shell"
+        - name: HOME
+          value: "/"
+        - name: TERM
+          value: "screen-256color"
+        - name: SHELL
+          value: "/system/bin/sh"
+        - name: ANDROID_BOOTLOGO
+          value: "1"
+        - name: TMPDIR
+          value: "/data/local/tmp"
+        - name: ANDROID_ASSETS
+          value: "/system/app"
+        - name: BOOTCLASSPATH
+          value: "/system/framework/core-oj.jar:/system/framework/core-libart.jar:/system/framework/conscrypt.jar:/system/framework/okhttp.jar:/system/framework/core-junit.jar:/system/framework/bouncycastle.jar:/system/framework/ext.jar:/system/framework/framework.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/system/framework/apache-xml.jar:/system/framework/org.apache.http.legacy.boot.jar"
+        - name: ASEC_MOUNTPOINT
+          value: "/mnt/asec"
+        - name: ANDROID_SOCKET_adbd
+          value: "7"
+        - name: HOSTNAME
+          value: "arm64"
+        - name: EXTERNAL_STORAGE
+          value: "/sdcard"
+        - name: ANDROID_STORAGE
+          value: "/storage"
+        - name: USER
+          value: "shell"
+        - name: SYSTEMSERVERCLASSPATH
+          value: "/system/framework/services.jar:/system/framework/ethernet-service.jar:/system/framework/wifi-service.jar"
+        imagePullPolicy: Never
+        command: ["/system/bin/sh", "-ce", "/anbox-init.sh"]
+        ports:
+        - containerPort: 5555
+        securityContext:
+          privileged: true
+          #capabilities:
+          #  add: [ "SYS_ADMIN", "NET_ADMIN", "SYS_MODULE", "SYS_NICE", "SYS_TIME", "SYS_TTY_CONFIG", "NET_BROADCAST", "IPC_LOCK", "SYS_RESOURCE" ]
+        volumeMounts:
+        - mountPath: /dev/binder:rw
+          name: volume-binder
+        - mountPath: /dev/ashmem:rw
+          name: volume-ashmem
+        - mountPath: /dev/fuse:rw
+          name: volume-fuse
+        - mountPath: /dev/qemu_pipe
+          name: volume-qemupipe
+        - mountPath: /dev/anbox_audio:rw
+          name: volume-anboxaudio
+        - mountPath: /dev/anbox_bridge:rw
+          name: volume-anboxbridge
+        - mountPath: /dev/input/event0:rw
+          name: volume-event0
+        - mountPath: /dev/input/event1:rw
+          name: volume-event1
+        - mountPath: /cache:rw
+          name: volume-cache
+        - mountPath: /data:rw
+          name: volume-data
+      volumes:
+      - name: volume-binder
+        hostPath:
+          path: /dev/binder${INSTANCE_NUM}
+      - name: volume-ashmem
+        hostPath:
+          path: /dev/ashmem
+      - name: volume-fuse
+        hostPath:
+          path: /dev/fuse
+      - name: volume-qemupipe
+        hostPath:
+          path: /run/user/1000/anbox/${INSTANCE_NUM}/sockets/qemu_pipe
+      - name: volume-anboxaudio
+        hostPath:
+          path: /run/user/1000/anbox/${INSTANCE_NUM}/sockets/anbox_audio
+      - name: volume-anboxbridge
+        hostPath:
+          path: /run/user/1000/anbox/${INSTANCE_NUM}/sockets/anbox_bridge
+      - name: volume-event0
+        hostPath:
+          path: /run/user/1000/anbox/${INSTANCE_NUM}/input/event0
+      - name: volume-event1
+        hostPath:
+          path: /run/user/1000/anbox/${INSTANCE_NUM}/input/event1
+      - name: volume-cache
+        hostPath:
+          path: /home/robox/anbox-data/${INSTANCE_NUM}/cache
+      - name: volume-data
+        hostPath:
+          path: /home/robox/anbox-data/${INSTANCE_NUM}/data
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: anbox
+spec:
+  selector:
+    app: android
+  ports:
+  - protocol: TCP
+    port: 8888
+    targetPort: 5555
+    nodePort: 31000
+  type: NodePort
+EOF
+)
+
+if [ "$1" = "a" ]; then
+echo "$ROBOX_TEMPLATE_YAML" | kubectl apply -f - > /dev/null
+else
+echo "$ROBOX_TEMPLATE_YAML" | kubectl delete -f - > /dev/null
+fi
+echo $?
+sleep 2
+watch -n 1 kubectl get node,pods,svc -o wide -n kube-system -n default
---
+++ src/use_cases/robox/setupenv/build_android.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+set -e
+
+ANDROID=anbox-work
+ROBOX=robox
+EXAGEAR=$ROBOX/Exagear
+
+echo "please set git user and email. e.g."
+echo git config --global user.email "you@example.com"
+echo git config --global user.name "Your Name"
+echo "make use both termial and git could accesss google.com, press enter to continue"
+read
+
+#if [ 0 -gt 1 ]; then
+echo "prepare environment"
+sudo sed -i "s/# deb-src/ deb-src/g" /etc/apt/sources.list
+sudo apt update
+sudo apt install -y openjdk-8-jdk
+sudo apt install -y libx11-dev libreadline6-dev libgl1-mesa-dev g++-multilib
+sudo apt install -y git flex bison gperf build-essential libncurses5-dev
+sudo apt install -y tofrodos python-markdown libxml2-utils xsltproc zlib1g-dev
+sudo apt install -y dpkg-dev libsdl1.2-dev
+sudo apt install -y git-core gnupg flex bison gperf build-essential
+sudo apt install -y zip curl zlib1g-dev gcc-multilib g++-multilib
+sudo apt install -y libc6-dev
+sudo apt install -y lib32ncurses5-dev x11proto-core-dev libx11-dev
+sudo apt install -y libgl1-mesa-dev libxml2-utils xsltproc unzip m4
+sudo apt install -y lib32z-dev ccache
+sudo apt install -y bc python flex bison gperf libsdl-dev build-essential zip curl
+
+if [ ! -d robox ]; then
+	echo "clone robox"
+	git clone https://github.com/kunpengcompute/robox.git
+else
+	echo "robox directory exist, skip, clone robox"
+fi
+
+echo "try to clone Android"
+if [ ! -x /usr/bin/repo ]; then
+	sudo apt install -y repo
+	#do not use the official repo because it it too new.
+#if [ ! -x ~/bin/repo ]; then
+#	echo "get repo"
+#	mkdir ~/bin
+#	PATH=~/bin:$PATH
+#	curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+#	chmod a+x ~/bin/repo
+#	echo "replace with python3"
+#	sed -i "s/#\!\/usr\/bin\/env python$/#\!\/usr\/bin\/env python3/g" ~/bin/repo
+else
+	echo "repo exist, skip"
+fi
+
+if [ ! -d anbox-work ]; then
+	echo "clone anbox android code"
+	mkdir -p anbox-work
+	cd anbox-work
+	repo init -u https://github.com/anbox/platform_manifests.git -b anbox
+	# total disk size is about 160G
+	repo sync -j16
+	echo "fallback the code"
+	cp -p robox/binaryFiles/snapshot20191206.xml anbox-work/.repo/manifests/
+	cd anbox-work
+	repo init -m snapshot20191206.xml
+	repo sync --force-sync frameworks/opt/net/wifi
+	repo sync -d -j16
+	cd -
+else
+	echo "anbox-work exist, skip to clone anbox android code"
+	echo "fallback the code locally"
+	cp -p robox/binaryFiles/snapshot20191206.xml anbox-work/.repo/manifests/
+	cd anbox-work
+	repo init -m snapshot20191206.xml
+	repo sync --force-sync frameworks/opt/net/wifi
+	repo sync -d -j16 --local-only
+	cd -
+fi
+
+echo "replace anbox code"
+rm -rf $ANDROID/vendor/anbox
+cp -rp $ROBOX $ANDROID/vendor/anbox
+#fi # if [ 0 -gt 1 ]; then
+
+echo "The exagear"
+cp -p $EXAGEAR/android/android-7.1.1_r13.patch $ANDROID
+cp -pr $EXAGEAR/android/vendor $ANDROID
+ls $ANDROID/vendor/
+#
+cp -pr $ROBOX/patch/android-7.1.1_r13-V1.0/* $ANDROID
+status=`cat script/status`
+if [ ! "PATCH_ANDROID_DONE" == $status ]; then
+	echo "Patching Android"
+	cp -pr $ROBOX/patch/patch.sh $ANDROID
+	cd $ANDROID
+	sh patch.sh
+	echo PATCH_ANDROID_DONE > script/status
+	cd -
+else
+	echo "Patch has already finished, skip patch"
+fi
+
+echo "build Android"
+cd $ANDROID
+#setup build environment
+source build/envsetup.sh
+#choose compile env
+lunch anbox_arm64-userdebug
+export JACK_EXTRA_CURL_OPTIONS=-k
+export LC_ALL=C
+make -j16
+
+echo "generate the android.img"
+cd vendor/anbox && ./scripts/create-package.sh $PWD/../../out/target/product/arm64/ramdisk.img $PWD/../../out/target/product/arm64/system.img
+ls -lh android.img
+
