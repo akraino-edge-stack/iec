4290b033c388eef12adc753427a2d6452f537991
rd-sw <rd-sw@ysemi.cn>
2022-04-27 13:02:07 +0000

    ROBOX：Upload robox scripts for IEC release 6
    RT
    
    Signed-off-by: rd-sw <rd-sw@ysemi.cn>
    Change-Id: I1695c71cb291273b926370bc9a84528107663ef0

---
+++ src/use_cases/robox/loadimages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+IMAGES_DIR=~/images
+IMAGE_MOUNT_DIR=${IMAGES_DIR}/images/android
+mkdir -p ${IMAGES_DIR}/images/android
+
+if [ ! -e /dev/binder1 ]; then
+    echo "try to insmod binder"
+    sudo modprobe ashmem_linux
+    sudo rmmod binder_linux || true; sudo modprobe binder_linux num_devices=254
+    sudo chmod 777 /dev/ashmem /dev/binder*
+else
+    echo "binder already loaded"
+fi
+
+if [[ `sudo docker images -a | grep robox` ]]; then
+    echo "robox images already loaded"
+else
+    sudo mount ${IMAGES_DIR}/android.img ${IMAGE_MOUNT_DIR}
+    cd ${IMAGE_MOUNT_DIR}
+    sudo tar --numeric-owner -cf- . | sudo docker import - android:robox
+    sleep 1
+    if [[ `sudo docker images -a | grep robox` ]]; then
+        echo "docker image loads successfully!"
+    fi
+fi
